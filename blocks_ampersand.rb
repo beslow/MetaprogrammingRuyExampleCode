d5091ff26f9ed8e2ec23ec57da6da7b73fd97a3f
beslow <549174542@qq.com>
2014-11-28 12:19:46 +0800

    Create blocks_ampersand.rb

---
+++ blocks_ampersand.rb
@@ -0,0 +1,13 @@
+def math(a, b)
+  yield(a, b)
+end
+
+def teach_math(a, b, &operation)
+  puts "Let's do the math:"
+  puts math(a, b, &operation)
+end
+
+teach_math(2, 3) {|x, y| x * y }
+
+=> Let's do the math:
+6
